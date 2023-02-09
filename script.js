0b8dcc07d54e9f71539dfa31d2fb55bcea5e5b0f
Edwin Gichuhi <edwingichuhis@gmai.com>
2023-02-09 12:58:07 +0300

    Add onclick to mobilemenu actions

---
+++ script.js
@@ -0,0 +1,9 @@
+const showMobileMenu = () => {
+  const menu = document.getElementById('mobile-menu');
+  menu.style.display = 'block';
+};
+
+const closeMobileMenu = () => {
+  const menu = document.getElementById('mobile-menu');
+  menu.style.display = 'none';
+};
