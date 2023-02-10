e3104362c345445824108b7ce781ce4a8b371837
Edwin Gichuhi <edwingichuhis@gmai.com>
2023-02-10 15:13:34 +0300

    Fix recent works mobile alignment

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const showMobileMenu = () => {
-  const menu = document.getElementById('mobile-menu');
-  menu.style.display = 'block';
-};
+// const showMobileMenu = () => {
+//   const menu = document.getElementById('mobile-menu');
+//   menu.style.display = 'block';
+// };
 
-const closeMobileMenu = () => {
-  const menu = document.getElementById('mobile-menu');
-  menu.style.display = 'none';
-};
+// const closeMobileMenu = () => {
+//   const menu = document.getElementById('mobile-menu');
+//   menu.style.display = 'none';
+// };
+
+const menu = document.getElementById('mobile-menu');
+menu.addEventListener('click', () => {
+  if (menu.style.display === 'none') {
+    menu.style.display = 'block';
+  } else {
+    menu.style.display = 'none';
+  }
+});
