d2b179a5ee94eaa72df3360d8d611030a4a76ce7
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-02-21 22:00:37 +0300

    Append card to card container

--- script.js
+++ script.js
@@ -21,3 +21,49 @@ mobileMenu.addEventListener('click', () => {
     mobileMenu.style.display = 'none';
   }
 });
+
+// const cards = {
+//   card_1: {
+//     title: 'First Project',
+//     description: 'First project description',
+//     image: '/imgs/MaskGroup.png',
+//   },
+// };
+
+const card = `
+                    <div class="card">
+                        <div class="mask-group">
+                            <div class="right-block">
+                                <div class="title">
+                                    Project Title
+                                </div>
+                                <div class="description">
+                                    A daily selection of privately personalized reads; no accounts or sign-ups required.
+                                    has
+                                    been the industry's standard
+                                </div>
+                                <ul class="tags">
+                                    <li class="html">
+                                        <span>html</span>
+                                    </li>
+                                    <li class="bootstrap">
+                                        <span>bootstrap</span>
+                                    </li>
+                                    <li class="ruby">
+                                        <span>Ruby</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="action">
+                            <a href="#">
+                                <button>
+                                    <h6>See Project</h6>
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+`;
+
+const cardsContainer = document.getElementById('cards-container');
+cardsContainer.innerHTML = card;
