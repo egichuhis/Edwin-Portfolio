a4a530fd0f95c728332f2f00c70ef8aaefa89c51
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-04-18 17:05:58 +0300

    refactor mobile view layout corrections

--- script.js
+++ script.js
@@ -3,17 +3,11 @@ const mobileMenu = document.getElementById('mobile-menu');
 const closeMenuIcon = document.getElementById('close-menu');
 
 menuIcon.addEventListener('click', () => {
-  if (mobileMenu.style.display === 'none') {
-    mobileMenu.style.display = 'block';
-  } else {
-    mobileMenu.style.display = 'none';
-  }
+  mobileMenu.style.display = 'block';
 });
 
 closeMenuIcon.addEventListener('click', () => {
-  if (mobileMenu.style.display === 'block') {
-    mobileMenu.style.display = 'none';
-  }
+  mobileMenu.style.display = 'none';
 });
 
 mobileMenu.addEventListener('click', () => {
